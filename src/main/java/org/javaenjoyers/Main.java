d54bd9859c9ff3f16509656f378fe4f660c692c0
gagoxyz <dgagoc@gmail.com>
2025-03-18 18:35:56 +0100

    Se añade usuario

--- src/main/java/org/javaenjoyers/Main.java
+++ src/main/java/org/javaenjoyers/Main.java
@@ -4,8 +4,6 @@ import org.javaenjoyers.controlador.Controlador;
 import org.javaenjoyers.modelo.*;
 import org.javaenjoyers.vista.Vista;
 
-import java.time.LocalDateTime;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -17,12 +15,13 @@ public class Main {
         // clases de prueba
         Cliente cliente01 = new Estandar("manuela@estandar.com", "Manuela", "c/cliente estandar S/N", "123456789");
         Cliente cliente02 = new Premium("david@premium.com", "David", "c/cliente estandar S/N", "87654321X");
+        Cliente cliente03 = new Premium("david@premium.com", "David", "c/cliente estandar S/N", "87654321X");
         Articulo articulo01 = new Articulo("A001", "Artículo de prueba", 20.99f, 4.99f, 60);
-        Pedido pedido01 = new Pedido(1, cliente01, articulo01, 1, LocalDateTime.now());
+        Pedido pedido01 = new Pedido(1, cliente01, articulo01, 1);
 
         // añadimos los clientes, articulos y pedidos a nuestra tienda
         modelo.agregarCliente(cliente01);
-        modelo.agregarCliente(cliente02);
+        modelo.agregarCliente(cliente03);
         modelo.agregarArticulo(articulo01);
         modelo.agregarPedido(pedido01);
 
